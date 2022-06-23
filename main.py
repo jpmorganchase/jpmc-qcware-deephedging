a4437dc21e0e4e2a4aacff1773fe79ba4850c1e0
SnehalRaj <snehalraj808@gmail.com>
2022-06-23 07:47:16 +0100

    refactor

--- main.py
+++ main.py
@@ -10,35 +10,37 @@ from qnn import linear, ortho_linear, ortho_linear_noisy
 from train import build_train_fn
 from loss_metrics import entropy_loss
 from data import gen_paths
-from utils import train_test_split, get_batches, HyperParams
+from utils import HyperParams
+import utils
 
 seed = 42
 key = jax.random.PRNGKey(seed)
 hps = HyperParams()
 
 # Data
-S = gen_paths(n_paths=120000, n_steps=hps.n_steps, seed=0)
-[S_train, S_test] = train_test_split([S], test_size=0.2)
-_, train_batches = get_batches(jnp.array(S_train[0]), batch_size=hps.batch_size)
+S = gen_paths(hps, seed=0)
+[S_train, S_test] = utils.train_test_split([S], test_size=hps.test_size)
+_, train_batches = utils.get_batches(
+    jnp.array(S_train[0]), batch_size=hps.batch_size)
 
-
-if hps.layer_type in ['linear','linear_svb']:
-  layer_func = linear
-elif hps.layer_type=='ortho':
-  layer_func = ortho_linear
-elif hps.layer_type=='noisy_ortho':
-  layer_func = partial(ortho_linear_noisy,noise_scale=0.01)
+if hps.layer_type in ['linear', 'linear_svb']:
+    layer_func = linear
+elif hps.layer_type == 'ortho':
+    layer_func = ortho_linear
+elif hps.layer_type == 'noisy_ortho':
+    layer_func = partial(ortho_linear_noisy, noise_scale=0.01)
 
 if hps.model_type == 'simple':
-  net = simple_network(hps=hps, layer_func=layer_func)
+    net = simple_network(hps=hps, layer_func=layer_func)
 elif hps.model_type == 'recurrent':
-  net = recurrent_network(hps=hps, layer_func=layer_func)
+    net = recurrent_network(hps=hps, layer_func=layer_func)
 elif hps.model_type == 'lstm':
-  net = lstm_network(hps=hps, layer_func=layer_func)
+    net = lstm_network(hps=hps, layer_func=layer_func)
 elif hps.model_type == 'attention':
-  net = attention_network(hps=hps, layer_func=layer_func)
+    net = attention_network(hps=hps, layer_func=layer_func)
 
-opt = optax.adam(1E-3)
+opt = utils.make_optimizer(optimizer=hps.optimizer,
+                           learning_rate=hps.learning_rate)
 key, init_key = jax.random.split(key)
 params, state, _ = net.init(init_key, (1, hps.n_steps+1, 1))
 opt_state = opt.init(params)
@@ -46,14 +48,15 @@ loss_metric = entropy_loss
 
 # Training
 
-train_fn, loss_fn = build_train_fn(hps, net, opt, loss_metric)
-num_epochs = 100
-loss = 0.0    
-with trange(1, num_epochs+1) as t:
-  for epoch in t:
-    for i, inputs in enumerate(train_batches):
-      inputs = inputs[...,None]
-      key, train_key = jax.random.split(key)
-      params, state, opt_state, loss, (wealths, deltas, outputs) = train_fn(
-          params, state, opt_state, train_key, inputs)
-    t.set_postfix(loss=loss, model=hps.model_type, layer=hps.layer_type, eps=hps.epsilon)
+train_fn, loss_fn = build_train_fn(
+    hps=hps, net=net, opt=opt, loss_metric=loss_metric)
+loss = 0.0
+with trange(1, hps.num_epochs+1) as t:
+    for epoch in t:
+        for i, inputs in enumerate(train_batches):
+            inputs = inputs[..., None]
+            key, train_key = jax.random.split(key)
+            params, state, opt_state, loss, (wealths, deltas, outputs) = train_fn(
+                params, state, opt_state, train_key, inputs)
+        t.set_postfix(loss=loss, model=hps.model_type,
+                      layer=hps.layer_type, eps=hps.epsilon)
