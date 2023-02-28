5fa69b9c81a5bededbef2665045dabf3370b4ca6
SnehalRaj <snehalraj808@gmail.com>
2023-02-28 11:04:21 +0100

    Moved hardware models from notebooks

--- models.py
+++ models.py
@@ -262,3 +262,111 @@ def attention_network(hps: HyperParams, layer_func=linear,  **kwargs) -> ModuleF
     net = sequential(*layers)
     return net
 
+
+
+## Layers below are for hardware implementaion
+
+def scan(f, init, xs, length=None):
+  if xs is None:
+    xs = [None] * length
+  carry = init
+  ys = []
+  for x in xs:
+    carry, y = f(carry, x)
+    ys.append(y)
+  return carry, jnp.stack(ys)
+
+def recurrent_network_hardware(hps, layer_func: ModuleFn = linear, **kwargs) -> ModuleFn:
+    """ Create a Recurrent Network.
+    Args:
+        n_features: The number of features.
+        n_layers: The number of layers.
+        layer_func: The type of layers to use.
+    """
+
+    preprocessing = [linear(hps.n_features), sigmoid]
+    features = hps.n_layers * [layer_func(hps.n_features), relu]
+    postprocessing = [linear(1), sigmoid]
+    layers = preprocessing + features + postprocessing
+    net = sequential(*layers)
+
+    def init_fn(key, inputs_shape):
+        params = net.init(
+            key, (inputs_shape[0], inputs_shape[1], 2*inputs_shape[2]))[0]
+        return params, None, inputs_shape
+
+    def apply_fn(params, state, key, inputs):
+        def cell_fn(prev_outputs, inputs):
+            inp = inputs[None, ...]
+            inp = jnp.concatenate([prev_outputs, inp], axis=-1)
+            delta = net.apply(params, None, key, inp)[0]
+            # print(f'inputs shape = {inp.shape} deltas shape = {delta.shape}')
+            return delta, delta
+
+        prev_state = jnp.zeros((1, inputs.shape[0], inputs.shape[-1]))
+        inputs = inputs.transpose(1, 0, 2)
+        _, outputs = scan(cell_fn, prev_state, inputs)
+        outputs = jnp.squeeze(outputs, 1)
+        outputs = outputs.transpose(1, 0, 2)
+        return outputs, state
+    return qnn.ModuleFn(apply_fn, init_fn)
+
+
+def lstm_cell_hardware(hps,  layer_func: ModuleFn = linear, **kwargs) -> ModuleFn:
+    """ Create an LSTM Cell.
+    Args:
+        n_features: The number of features.
+        layer_func: The type of layers to use.
+    """
+
+    _linear = layer_func(n_features=int(hps.n_features/2), with_bias=True)
+
+    def init_fn(key, inputs_shape):
+        keys = jax.random.split(key, num=4)
+        params = {}
+        layer_idx = ['i', 'g', 'f', 'o']
+        _shape = (inputs_shape[0], inputs_shape[1], 2*inputs_shape[2])
+        _init_params = {}
+        for i, id in enumerate(layer_idx):
+            _init_params[id] = _linear.init(keys[i],  _shape)[0]
+            params.update(qnn.add_scope_to_params(id, _init_params[id]))
+        return params, None, inputs_shape
+
+    def apply_fn(params, state, key, inputs):
+        def cell_fn(prev_state, inputs):
+            prev_hidden, prev_cell = prev_state
+            x_and_h = jnp.concatenate([inputs, prev_hidden], axis=-1)
+            layer_idx = ['i', 'g', 'f', 'o']
+            _apply_params = {}
+            for i, id in enumerate(layer_idx):
+                _apply_params[id] = qnn.get_params_by_scope(id, params)
+            i = _linear.apply(_apply_params['i'], None, key, x_and_h)[0]
+            g = _linear.apply(_apply_params['g'], None, key, x_and_h)[0]
+            f = _linear.apply(_apply_params['f'], None, key, x_and_h)[0]
+            o = _linear.apply(_apply_params['o'], None, key, x_and_h)[0]
+            # i = input, g = cell_gate, f = forget_gate, o = output_gate
+            f = jax.nn.sigmoid(f + 1)
+            c = f * prev_cell + jax.nn.sigmoid(i) * jnp.tanh(g)
+            h = jax.nn.sigmoid(o) * jnp.tanh(c)
+            return jnp.stack([h, c], axis=0), h
+
+        prev_state = jnp.zeros((2, inputs.shape[0], inputs.shape[-1]))
+        inputs = inputs.transpose(1, 0, 2)
+        _, outputs = scan(cell_fn, prev_state, inputs)
+        outputs = outputs.transpose(1, 0, 2)
+        return outputs, state
+    return qnn.ModuleFn(apply_fn, init_fn)
+
+
+def lstm_network_hardware(hps, layer_func: ModuleFn = linear, **kwargs) -> ModuleFn:
+    """ Create an LSTM Network.
+    Args:
+        n_features: The number of features.
+        layer_func: The type of layers to use.
+    """
+    preprocessing = [linear( int(hps.n_features/2) ), sigmoid]
+    features = [lstm_cell_hardware(hps=hps, layer_func=layer_func)]
+    postprocessing = [linear(1), sigmoid]
+    layers = preprocessing + features + postprocessing
+    net = sequential(*layers)
+    return net
