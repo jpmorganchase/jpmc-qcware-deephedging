91ebebc651223ee19ba84a2f6aad9451dbd9e9ff
El Amine Cherrat <45946838+qdevpsi3@users.noreply.github.com>
2023-03-03 17:13:13 +0100

    remove qcware_transpile and qio

--- Part_2/qcware_transpile/dialects/pyzx/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Pyzx is a toolkit for manipulating circuits using the ZX
-calculus.
-"""
-
-from .pyzx_dialect import (dialect, native_to_ir, ir_to_native,
-                             native_circuits_are_equivalent, audit)
--- Part_2/qcware_transpile/dialects/qsharp/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-QSharp is Microsoft's toolkit.  Initial interaction will be somewhat limited.
-"""
-from .qsharp_dialect import (dialect, ir_to_native,
-                              ) #, audit)
